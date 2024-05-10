f6f294ff7cd2918a4d6290e496777f185b8bec79
brewchetta <chett.tiller@gmail.com>
2024-05-10 10:55:41 -0400

    omg flask

--- server/app.py
+++ server/app.py
@@ -7,7 +7,25 @@ app.json.compact = False
 
 @app.get('/')
 def index():
-    return make_response( jsonify("Hello world") )
+    return make_response( "<h1>Hello World</h1>" )
+
+@app.get('/store')
+def store():
+    return { "hamburger": { "price": 12.00} }
+
+@app.get('/other-site')
+def stuff():
+    return "<a href='http://www.reddit.com'>Go to reddit</a>"
+
+@app.get('/blog/<string:name>')
+def show_blog(name:str):
+    return f"{name} has an amazing blog check it out"
+
+@app.get('/math/<int:number_one>/<int:number_two>')
+def add(number_one, number_two):
+    return make_response( jsonify( {"result": number_one + number_two} ) )
+
+# @app.get('/raccoons/<int:id>')
 
 # write your routes here!
 
--- server/models.py
+++ server/models.py
@@ -1,1 +1,3 @@
 # ooooh look at this mysterious file! I bet we'll fill it with sqlalchemy later :D
+
+from app import app
