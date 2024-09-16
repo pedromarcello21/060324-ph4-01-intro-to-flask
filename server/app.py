2edd6618a4f0647cd657d0faac9fd8bf58986190
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-09-16 09:23:33 -0400

    latest

--- server/app.py
+++ server/app.py
@@ -7,6 +7,22 @@ from config import app
 def index():
     return make_response( "<h1>Hello World</h1>" )
 
+@app.get('/greeting')
+def greeting():
+    greet = {"response": "Hello!"}
+    return greet
+
+@app.get('/count-to/<int:x>')
+def count(x):
+    list = []
+    for i in range(x):
+        list.append(i+1)
+    return list
+
+@app.get('/lower-case/<string:incoming_string>')
+def lower_case(incoming_string):
+    return {"result":incoming_string.lower()}
+
 # write your routes here!
 
 if __name__ == '__main__':
